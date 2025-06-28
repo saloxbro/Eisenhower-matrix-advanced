11fb6763e048efbfa062776a6ad020c9b30f08e3
saloxbro <bitf24m044@pucit.edu.pk>
2025-06-28 11:51:51 +0500

    Configure project for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,13 @@
+// vite.config.js
+
 export default {
+  // Keep the source code root as 'src'
   root: 'src',
+
+  build: {
+    // Set the output directory to go one level up from 'src' to the project root.
+    outDir: '../dist',
+    // It's good practice to ensure the output directory is cleared on each build.
+    emptyOutDir: true,
+  }
 };
