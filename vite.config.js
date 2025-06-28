183db986f0ec8fbfb4c917251c8cd53b9f375a95
M.Salman Shahid <bitf24m044@pucit.edu.pk>
2025-06-28 11:42:51 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
+import { resolve } from 'path';
+
 export default {
   root: 'src',
+  build: {
+    // This tells Vite to build the project into a `dist` folder in the project root.
+    outDir: resolve(__dirname, '..', 'dist'),
+    // Ensure the output directory is cleared before each build.
+    emptyOutDir: true,
+  },
 };
